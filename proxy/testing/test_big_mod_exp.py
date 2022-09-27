a6fb56a59512965075a746ee6f69261a100fc826
sinev-valentine <sinev-valentine@yandex.ru>
2022-09-27 14:34:53 +0300

    fix last commit

--- proxy/testing/test_big_mod_exp.py
+++ proxy/testing/test_big_mod_exp.py
@@ -99,7 +99,10 @@ class Test_big_mod_exp(unittest.TestCase):
         print("\ntest_big_mod_exp")
         number = self.storage_contract.functions.modExp(5, 2, 7).call()
 
-        tx = self.storage_contract.functions.modExp(5, 2, 7).buildTransaction()
+
+        nonce = self.proxy.eth.get_transaction_count(eth_account.address)
+        tx = {'nonce': nonce}
+        tx = self.storage_contract.functions.modExp(5, 2, 7).buildTransaction(tx)
         tx = proxy.eth.account.sign_transaction(tx, eth_account.key)
         tx_hash = proxy.eth.send_raw_transaction(tx.rawTransaction)
         tx_receipt = proxy.eth.wait_for_transaction_receipt(tx_hash)
