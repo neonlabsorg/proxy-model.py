a0c84cca8bb9441b75453bf089adf9e43e44fe6f
sinev-valentine <sinev-valentine@yandex.ru>
2022-10-11 12:10:57 +0300

    fix

--- proxy/testing/test_big_mod_exp.py
+++ proxy/testing/test_big_mod_exp.py
@@ -173,15 +173,11 @@ class Test_big_mod_exp(unittest.TestCase):
 
     def test_big_mod_exp(self):
         for test in TEST_DATA:
-            # b = int(test["Base"], 16)
-            # e = int(test["Exponent"], 16)
-            # m = int(test["Modulus"], 16)
-            # r = int(test["Expected"], 16)
-
-            b = bytes.fromhex(test["Base"])
-            e = bytes.fromhex(test["Exponent"])
-            m = bytes.fromhex(test["Modulus"])
+            b = int(test["Base"], 16)
+            e = int(test["Exponent"], 16)
+            m = int(test["Modulus"], 16)
             r = int(test["Expected"], 16)
-            self.call_contract(b, e, m, r)
+
+            self.call_contract(b.to_bytes(), e.to_bytes(), m.to_bytes(), r)
 
 
