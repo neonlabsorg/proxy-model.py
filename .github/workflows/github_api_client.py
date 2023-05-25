eb8ccc1f40478f94827d22b99f5a780ccd12f5c5
kristinaNikolaeva <kn@neonlabs.org>
2023-05-25 12:34:44 +0300

    fix after review

--- .github/workflows/github_api_client.py
+++ .github/workflows/github_api_client.py
@@ -1,9 +1,10 @@
 import click
 import requests
 
+NEON_TESTS_ENDPOINT = "https://api.github.com/repos/neonlabsorg/neon-tests"
+
 
 class GithubClient():
-    TESTS_ENDPOINT = "https://api.github.com/repos/neonlabsorg/neon-tests"
 
     def __init__(self, token):
         self.headers = {"Authorization": f"Bearer {token}",
@@ -11,7 +12,7 @@ class GithubClient():
 
     def get_dapps_runs_list(self, branch="develop"):
         response = requests.get(
-            f"{self.TESTS_ENDPOINT}/actions/workflows/dapps.yml/runs?branch={branch}", headers=self.headers)
+            f"{NEON_TESTS_ENDPOINT}/actions/workflows/dapps.yml/runs?branch={branch}", headers=self.headers)
         if response.status_code != 200:
             raise RuntimeError(f"Can't get dapps tests runs list. Error: {response.json()}")
         runs = [item['id'] for item in response.json()['workflow_runs']]
@@ -19,7 +20,7 @@ class GithubClient():
 
     def get_dapps_runs_count(self, branch="develop"):
         response = requests.get(
-            f"{self.TESTS_ENDPOINT}/actions/workflows/dapps.yml/runs?branch={branch}", headers=self.headers)
+            f"{NEON_TESTS_ENDPOINT}/actions/workflows/dapps.yml/runs?branch={branch}", headers=self.headers)
         return int(response.json()["total_count"])
 
     def run_dapps_dispatches(self, proxy_url, solana_url, faucet_url, network_id='111', branch='develop'):
@@ -32,7 +33,7 @@ class GithubClient():
                            "network": "custom"}
                 }
         response = requests.post(
-            f"{self.TESTS_ENDPOINT}/actions/workflows/dapps.yml/dispatches", json=data, headers=self.headers)
+            f"{NEON_TESTS_ENDPOINT}/actions/workflows/dapps.yml/dispatches", json=data, headers=self.headers)
         click.echo(f"Sent data: {data}")
         click.echo(f"Headers: {self.headers}")
         click.echo(f"Status code: {response.status_code}")
@@ -41,5 +42,5 @@ class GithubClient():
 
     def get_dapps_run_info(self, id):
         response = requests.get(
-            f"{self.TESTS_ENDPOINT}/actions/runs/{id}", headers=self.headers)
+            f"{NEON_TESTS_ENDPOINT}/actions/runs/{id}", headers=self.headers)
         return response.json()
