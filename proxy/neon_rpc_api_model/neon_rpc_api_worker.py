d93c707ee2c52d0d94a7b86ee389e1076a87db9f
Andrey Falaleev <andrew.falaleev@gmail.com>
2023-12-22 23:38:29 +0700

    NDEV-2473 neon-rpc-api: input params validations in eth_call

--- proxy/neon_rpc_api_model/neon_rpc_api_worker.py
+++ proxy/neon_rpc_api_model/neon_rpc_api_worker.py
@@ -637,12 +637,14 @@ class NeonRpcApiWorker:
         if not isinstance(obj, dict):
             raise InvalidParamError(message='invalid object type')
 
-        if not obj['data']:
+        if not obj.get('data'):
             raise InvalidParamError(message="missing data")
+
+        contract = obj.get('to')
+        contract = self._normalize_address(contract, 'to-address')
 
         try:
             sender = obj.get('from')
-            contract = obj.get('to')
             data = obj.get('data')
             value = obj.get('value')
             gas_limit = obj.get('gas')
