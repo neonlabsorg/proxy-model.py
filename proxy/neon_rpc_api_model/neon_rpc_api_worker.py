2587bae83ce2888c33bdfa121567226e1da2d022
Andrey Falaleev <andrew.falaleev@gmail.com>
2024-01-01 13:53:09 +0000

    set-version: v1.4.7

--- proxy/neon_rpc_api_model/neon_rpc_api_worker.py
+++ proxy/neon_rpc_api_model/neon_rpc_api_worker.py
@@ -46,7 +46,7 @@ from .estimate import GasEstimate
 from .transaction_validator import NeonTxValidator
 
 
-NEON_PROXY_PKG_VERSION = '1.4.6'
+NEON_PROXY_PKG_VERSION = '1.4.7'
 NEON_PROXY_REVISION = 'NEON_PROXY_REVISION_TO_BE_REPLACED'
 LOG = logging.getLogger(__name__)
 
